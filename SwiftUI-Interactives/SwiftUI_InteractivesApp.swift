3649e319686bc9b9432e79a5fa056bc146a1347b
Hiren Rafaliya <hiren6997@gmail.com>
2024-03-23 20:05:43 +0530

    Initial Commit

---
+++ SwiftUI-Interactives/SwiftUI_InteractivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_InteractivesApp.swift
+//  SwiftUI-Interactives
+//
+//  Created by Hiren Rafaliya on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_InteractivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
